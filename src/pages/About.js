7a82c87cebdeae38f671ad10b88cbe4f92c3527f
Parnom1234 <mergerobot123@gmail.com>
2025-01-03 09:52:11 +0700

    deploy

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,71 +14,32 @@ const About = () => {
         }}
       >
         <div className="container">
-          <h1
-            className="display-4"
-            style={{
-              fontFamily: "'Poppins', sans-serif",
-              fontWeight: "600",
-            }}
-          >
+          <h1 className="display-4" style={{ fontFamily: "'Poppins', sans-serif", fontWeight: "600" }}>
             เกี่ยวกับเรา
           </h1>
-          <p
-            className="lead"
-            style={{
-              fontSize: "1.3rem",
-              fontWeight: "400",
-              maxWidth: "800px",
-              margin: "auto",
-            }}
-          >
-            วิทยาลัยเทคโนโลยีโปลิเทคนิคลานนา เชียงใหม่ สาขาเทคโนโลยีสารสนเทศ
-            - สร้างอนาคตด้วยนวัตกรรมที่ไม่มีสิ้นสุด
+          <p className="lead" style={{ fontSize: "1.3rem", fontWeight: "400", maxWidth: "800px", margin: "auto" }}>
+            วิทยาลัยเทคโนโลยีโปลิเทคนิคลานนา เชียงใหม่ สาขาเทคโนโลยีสารสนเทศ - สร้างอนาคตด้วยนวัตกรรมที่ไม่มีสิ้นสุด
           </p>
         </div>
       </section>
 
       {/* About Us Section */}
-      <section
-        className="about-us-section"
-        style={{
-          padding: "60px 0",
-          backgroundColor: "#f7f7f7",
-        }}
-      >
+      <section className="about-us-section" style={{ padding: "60px 0", backgroundColor: "#f7f7f7" }}>
         <div className="container">
-          <h2
-            className="text-center mb-5"
-            style={{
-              color: "#2d3748",
-              fontFamily: "'Poppins', sans-serif",
-              fontWeight: "600",
-            }}
-          >
+          <h2 className="text-center mb-5" style={{ color: "#2d3748", fontFamily: "'Poppins', sans-serif", fontWeight: "600" }}>
             ภารกิจของเรา
           </h2>
           <div className="row align-items-center">
             <div className="col-md-6">
               <img
-                src="https://scontent.fcnx1-1.fna.fbcdn.net/v/t39.30808-6/459572266_1038987961560629_5992916616841721678_n.jpg?stp=dst-jpg_s600x600_tt6&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=cP6DHXtBykkQ7kNvgE9C97o&_nc_oc=AdgSyKBOpAZlQK--X6B1M3PRfbEL7mNUbgKLhMDfMoM67XgBTlanNQE0Wi1XdVIgXXA&_nc_zt=23&_nc_ht=scontent.fcnx1-1.fna&_nc_gid=AQQbH_fxuPJdy6FF2uSYWsK&oh=00_AYDUXlpxTbc3BFO_qhOqjkWPvuCZ6ZZPTLL1dy7mBN6FZQ&oe=6773FE22"
+                src="https://scontent.fcnx1-1.fna.fbcdn.net/v/t39.30808-6/471482133_1095496179038494_5561592027701869317_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=O1ifUz943roQ7kNvgGmoJqs&_nc_oc=AdikBBOI9Vb1GDpxe48EEFOvzyFkKItKh3tQQhBDtImnVPd6Hfj5qy5cnC-xQnLIFBM&_nc_zt=23&_nc_ht=scontent.fcnx1-1.fna&_nc_gid=Awb1nxgJ1bui8qr5Gykb_k2&oh=00_AYBEH6rsQAJejDTnMW6pu1AN_SrDOnmAoatxU1HOD_X3bg&oe=677D28C0"
                 alt="About Us"
                 className="img-fluid rounded shadow-lg"
-                style={{
-                  objectFit: "cover",
-                  height: "400px",
-                  width: "100%",
-                }}
+                style={{ objectFit: "cover", height: "400px", width: "100%" }}
               />
             </div>
             <div className="col-md-6">
-              <p
-                style={{
-                  fontSize: "1.1rem",
-                  color: "#333",
-                  lineHeight: "1.6",
-                  fontFamily: "'Roboto', sans-serif",
-                }}
-              >
+              <p style={{ fontSize: "1.1rem", color: "#333", lineHeight: "1.6", fontFamily: "'Roboto', sans-serif" }}>
                 สาขาเทคโนโลยีสารสนเทศมุ่งเน้นการพัฒนาและสร้างบัณฑิตที่มีความรู้ความสามารถ
                 เพื่อรองรับการเปลี่ยนแปลงของโลกเทคโนโลยีที่ไม่มีวันหยุดนิ่ง
                 เราพร้อมที่จะผลักดันนักศึกษาให้กลายเป็นผู้สร้างสรรค์แห่งยุคใหม่
@@ -89,34 +50,14 @@ const About = () => {
       </section>
 
       {/* History Section */}
-      <section
-        className="history-section"
-        style={{
-          backgroundColor: "#fffbf0",
-          padding: "60px 0",
-        }}
-      >
+      <section className="history-section" style={{ backgroundColor: "#fffbf0", padding: "60px 0" }}>
         <div className="container">
-          <h2
-            className="text-center mb-4"
-            style={{
-              color: "#2d3748",
-              fontFamily: "'Poppins', sans-serif",
-              fontWeight: "600",
-            }}
-          >
+          <h2 className="text-center mb-4" style={{ color: "#2d3748", fontFamily: "'Poppins', sans-serif", fontWeight: "600" }}>
             ประวัติความเป็นมา
           </h2>
           <div className="row align-items-center">
             <div className="col-md-6">
-              <p
-                style={{
-                  fontSize: "1.1rem",
-                  color: "#333",
-                  lineHeight: "1.7",
-                  fontFamily: "'Roboto', sans-serif",
-                }}
-              >
+              <p style={{ fontSize: "1.1rem", color: "#333", lineHeight: "1.7", fontFamily: "'Roboto', sans-serif" }}>
                 วิทยาลัยก่อตั้งขึ้นในปี พ.ศ. 2538 ด้วยความมุ่งมั่นที่จะสร้างสรรค์เทคโนโลยีที่ทันสมัย
                 และช่วยยกระดับชุมชนผ่านการพัฒนานวัตกรรม โดยมุ่งเน้นที่คุณภาพการศึกษา
                 และการผลิตบัณฑิตที่พร้อมตอบสนองความต้องการของตลาดแรงงาน
@@ -124,14 +65,10 @@ const About = () => {
             </div>
             <div className="col-md-6">
               <img
-                src="https://www.lannapoly.ac.th/poly_alumni/img/2565.jpg"
+                src="https://online.anyflip.com/kagil/accountlogo.jpg"
                 alt="History"
                 className="img-fluid rounded shadow-lg"
-                style={{
-                  objectFit: "cover",
-                  height: "400px",
-                  width: "100%",
-                }}
+                style={{ objectFit: "cover", height: "400px", width: "100%" }}
               />
             </div>
           </div>
@@ -139,101 +76,33 @@ const About = () => {
       </section>
 
       {/* Vision Section */}
-      <section
-        className="vision-section"
-        style={{
-          backgroundColor: "#333",
-          color: "#fff",
-          padding: "60px 0",
-        }}
-      >
+      <section className="vision-section" style={{ backgroundColor: "#333", color: "#fff", padding: "60px 0" }}>
         <div className="container">
-          <h2
-            className="text-center mb-5"
-            style={{
-              color: "#fff",
-              fontFamily: "'Poppins', sans-serif",
-              fontWeight: "600",
-            }}
-          >
+          <h2 className="text-center mb-5" style={{ color: "#fff", fontFamily: "'Poppins', sans-serif", fontWeight: "600" }}>
             วิสัยทัศน์ของเรา
           </h2>
-          <div
-            id="visionCarousel"
-            className="carousel slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img
-                  src="https://i.ytimg.com/vi/OyLcgnQRg5k/maxresdefault.jpg"
-                  className="d-block w-100"
-                  alt="Vision 1"
-                />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>วิสัยทัศน์ที่ 1</h5>
-                  <p>
-                    เรามุ่งมั่นที่จะเป็นผู้นำด้านเทคโนโลยีสารสนเทศ ที่สร้างสรรค์นวัตกรรมเพื่อพัฒนาสังคม
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="https://www.cmthainews.com/wp-content/uploads/2022/05/S__16703590.jpg"
-                  className="d-block w-100"
-                  alt="Vision 2"
-                />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>วิสัยทัศน์ที่ 2</h5>
-                  <p>เราจะส่งเสริมการเรียนรู้และพัฒนาทักษะการทำงานที่สามารถนำไปใช้ในชีวิตจริงได้</p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="https://mpics.mgronline.com/pics/Images/566000005762504.JPEG"
-                  className="d-block w-100"
-                  alt="Vision 3"
-                />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>วิสัยทัศน์ที่ 3</h5>
-                  <p>
-                    เรามุ่งเน้นการสร้างบัณฑิตที่มีทักษะสูงและพร้อมที่จะรับมือกับการเปลี่ยนแปลงของโลกในยุคดิจิทัล
-                  </p>
-                </div>
-              </div>
+          <div className="row">
+            <div className="col-md-4 text-center">
+              <img src="https://scontent.fcnx3-1.fna.fbcdn.net/v/t39.30808-6/455245497_1016045353854890_8442259950307203794_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=CQPEB4H8MfsQ7kNvgE8KrWQ&_nc_oc=AdioQ2UKD17phipndtUVCD-A_CsA9O-WXM92nLTl3i0cw57Bw_sYDtIjyKSGH3FIcbA&_nc_zt=23&_nc_ht=scontent.fcnx3-1.fna&_nc_gid=ANIQ1ppIE3ucHj2r7IgBLHD&oh=00_AYA_EXl0R0_fKkfgUW5LzJelbM1Rnl_HLJfEXtYmcRM_OA&oe=677D380E" alt="Vision 1" className="img-fluid rounded-circle mb-3" />
+              <h5>วิสัยทัศน์ที่ 1</h5>
+              <p>เรามุ่งมั่นที่จะเป็นผู้นำด้านเทคโนโลยีสารสนเทศ ที่สร้างสรรค์นวัตกรรมเพื่อพัฒนาสังคม</p>
             </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#visionCarousel"
-              data-bs-slide="prev"
-            >
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#visionCarousel"
-              data-bs-slide="next"
-            >
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
+            <div className="col-md-4 text-center">
+              <img src="https://scontent.fcnx3-1.fna.fbcdn.net/v/t39.30808-6/471664777_1095028025751976_3845432151327842635_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=GE_5b2DhtjgQ7kNvgEYCTIH&_nc_oc=AdgwjKjVW8IhWYmn_HkRyQLRE4OQVMPs8SmY1ZhBu1xTvxVaHIDc08V5YdOAvUpCOCo&_nc_zt=23&_nc_ht=scontent.fcnx3-1.fna&_nc_gid=A-2VXlfUaSnL5uK07JUc9Y3&oh=00_AYBXKfy3u4G5dEX-Qmp-yHhiL3i6ZaEJPqYKFPhDnYIdEA&oe=677D205B" alt="Vision 2" className="img-fluid rounded-circle mb-3" />
+              <h5>วิสัยทัศน์ที่ 2</h5>
+              <p>เราจะส่งเสริมการเรียนรู้และพัฒนาทักษะการทำงานที่สามารถนำไปใช้ในชีวิตจริงได้</p>
+            </div>
+            <div className="col-md-4 text-center">
+              <img src="https://scontent.fcnx3-1.fna.fbcdn.net/v/t39.30808-6/471236050_1119620743497350_7781311657508832092_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=ljDcNgK1b90Q7kNvgF0j5ah&_nc_oc=AdiL0BUi5lRfcEDGTqbCDH_Si9FKnZJehOgPebwMDtYVETkIj9DiG4Ys35f2vNb1k98&_nc_zt=23&_nc_ht=scontent.fcnx3-1.fna&_nc_gid=AQUshfhqUl7LYKH6ftfrB-G&oh=00_AYBgaHIUksvWt8U1IqI5yDW-G5DC5MQ_9GTw6O-QId_MrQ&oe=677D3C83" alt="Vision 3" className="img-fluid rounded-circle mb-3" />
+              <h5>วิสัยทัศน์ที่ 3</h5>
+              <p>เรามุ่งเน้นการสร้างบัณฑิตที่มีทักษะสูงและพร้อมที่จะรับมือกับการเปลี่ยนแปลงของโลกในยุคดิจิทัล</p>
+            </div>
           </div>
         </div>
       </section>
 
       {/* Call to Action Section */}
-      <section
-        className="cta-section"
-        style={{
-          backgroundColor: "#4caf50",
-          color: "#fff",
-          padding: "60px 0",
-          textAlign: "center",
-        }}
-      >
+      <section className="cta-section" style={{ backgroundColor: "#4caf50", color: "#fff", padding: "60px 0", textAlign: "center" }}>
         <div className="container">
           <h2 className="mb-4">พร้อมที่จะก้าวไปกับเรา?</h2>
           <p>สมัครเรียนวันนี้ หรือเข้าร่วมกิจกรรมของเราเพื่อสัมผัสประสบการณ์การเรียนรู้ที่แตกต่าง!</p>
@@ -247,4 +116,3 @@ const About = () => {
 };
 
 export default About;
- 
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -18,16 +18,15 @@ const Courses = () => {
       title: "ประกาศนียบัตรวิชาชีพ (ปวช.)",
       description: "หลักสูตรที่มุ่งเน้นการพัฒนาทักษะพื้นฐานด้านเทคโนโลยีสารสนเทศ...",
       details: "หลักสูตรนี้เน้นการเรียนรู้พื้นฐานที่สำคัญของเทคโนโลยีสารสนเทศ...",
-      imgUrl: "https://path-to-image.com",  // ใส่ URL รูปภาพที่แท้จริง
+      imgUrl: "https://scontent.fcnx1-1.fna.fbcdn.net/v/t39.30808-6/466679249_1066713731916739_6371523822895452620_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=Dtb5QNm4jNwQ7kNvgHHmf75&_nc_oc=Adjt3aoXUpeuBA64lVBrsNi6YWKpPZk76-BLjTTIW3DWXTNJWuZF3owZ554RM-PBvkI&_nc_zt=23&_nc_ht=scontent.fcnx1-1.fna&_nc_gid=AcSKV4LRFa6R9zoHm78_Qs6&oh=00_AYAHpSTQjBppIsYwS6Pu0qoff6Bl1DJqDtDktuNwb46Uaw&oe=677D267D",
     },
     {
       id: 2,
       title: "ประกาศนียบัตรวิชาชีพชั้นสูง (ปวส.)",
       description: "หลักสูตรที่เน้นการฝึกฝนทักษะขั้นสูงในด้านเทคโนโลยีสารสนเทศ...",
       details: "หลักสูตรนี้มุ่งเน้นการพัฒนาทักษะที่สูงขึ้นในด้านเทคโนโลยีสารสนเทศ...",
-      imgUrl: "https://path-to-image.com",  // ใส่ URL รูปภาพที่แท้จริง
+      imgUrl: "https://scontent.fcnx1-1.fna.fbcdn.net/v/t39.30808-6/467157927_1066713808583398_5751301120261094833_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_ohc=GuEzFp8LmI4Q7kNvgHewSoG&_nc_oc=AdjcDMi3yNsIv_eFalvb7WOsFHOyhkj-LKC00QYvmOtTCqGr_1cwC14og_8unw1uqSs&_nc_zt=23&_nc_ht=scontent.fcnx1-1.fna&_nc_gid=AUU0q5AoOUQNLhAp-VtF4-c&oh=00_AYCD6TaDMzr0ccfQztxAvQ7gGeHLyFZQGxpbrT0a1qvvJw&oe=677D1A29",
     },
-    // เพิ่มข้อมูลหลักสูตรเพิ่มเติมที่นี่
   ];
 
   return (
@@ -37,11 +36,11 @@ const Courses = () => {
           <h1 className="text-center mb-4" style={{ color: "#4caf50", fontWeight: "600" }}>
             หลักสูตรที่เราเปิดสอน
           </h1>
-          <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div className="row justify-content-center">
             {courses.map((course) => (
-              <div className="col" key={course.id}>
+              <div className="col-md-6 col-lg-4 mb-4" key={course.id}>
                 <div
-                  className="card h-100 shadow-lg border-0"
+                  className="card h-100 shadow-lg border-0 d-flex flex-column justify-content-center align-items-center text-center"
                   style={{ cursor: "pointer" }}
                   onClick={() => handleShowModal(course)}
                 >
@@ -53,6 +52,7 @@ const Courses = () => {
                       transition: "transform 0.3s ease",
                       objectFit: "cover",
                       height: "200px",
+                      width: "100%",
                     }}
                   />
                   <div className="card-body">
